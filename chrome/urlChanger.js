511318064dc9d4b859c794082833f64044b09169
Zexuz <Robin.edbom@gmail.com>
2016-08-19 02:05:49 +0200

    Fixed problem where there was no "/all" page and it would return a 404

--- chrome/urlChanger.js
+++ chrome/urlChanger.js
@@ -1,9 +1,32 @@
 function doStuff() {
 
-    if (window.location.href.substr(window.location.href.length - 4).indexOf("all") > -1)
-        return;
-    window.location.href += "all/";
+    if (window.location.href.substr(window.location.href.length - 4).indexOf("all") === -1) {
+        httpGetAsync(window.location.href + "all/", function (err) {
+            console.log(err);
+            if (err) return;
+
+            window.location.href += "all/";
+        });
+    }
 }
+
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+
+        if (xmlHttp.readyState !== 4)
+            return;
+
+        if (xmlHttp.status == 404)
+            return callback(true);
+
+        callback(false);
+
+    };
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
 
 doStuff();
 
